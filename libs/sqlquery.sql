11f99f59c63bca2b11922acf2af55ca0491fa1eb
etv servert uploader <github@nijhu.nl>
2020-11-13 16:10:43 +0100

    remove sqlquery

--- libs/sqlquery.sql
+++
@@ -1,9 +0,0 @@
--- SQLite
-
---INSERT INTO VNF (service, srcIP, dstIP, srcPort, dstPort, prio) VALUES ('test1', '10.0.1.2', '10.0.1.20', '500', '500', 1);
-
---INSERT INTO VNF (service, srcIP, dstIP, srcPort, dstPort, prio) VALUES ('test2', '10.0.1.2', '10.0.1.20', '500', '500', -100);
---INSERT INTO VNF (service, srcIP, dstIP, srcPort, dstPort, prio) VALUES ('test2', '10.0.1.2', '10.0.1.20', '500', '500', -5);
-
-select * from VNF where srcIP IN ('ALL', '10.0.1.10');
-
